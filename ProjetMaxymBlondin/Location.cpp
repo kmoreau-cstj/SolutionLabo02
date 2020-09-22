2ccc18803268c37185b90e8d0a4b9e7ef22a12ca
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-21 21:38:14 -0400

    Correction

--- ProjetMaxymBlondin/Location.cpp
+++ ProjetMaxymBlondin/Location.cpp
@@ -1,4 +1,4 @@
-// But : Le programme calcule le co˚t de location d'un vÈhicule
+// But : Le programme calcule le co√ªt de location d'un v√©hicule
 // Auteur : Maxym Blondin
 // Date : 2020-09-17
 
@@ -6,50 +6,68 @@
 
 int main()
 {
-	setlocale(LC_ALL, "");			// Convertir correctement les accents de Windows ‡ la console
+	setlocale(LC_ALL, "");			// Convertir correctement les accents de Windows √† la console
 
-	int jour;		//IntÈgration des variables
-	int kil;		//IntÈgration des variables
-	int cout = 45;	//IntÈgration des variables
+	int jour;		//KM : Non pas int√©gration mais d√©claration : Int√©gration des variables
+	int kil;		//Int√©gration des variables
+	int cout = 45;	//Int√©gration des variables
 
 
-	std::cout << "Veuillez entrer le nombre de jours que vous avez louÈ l'automobile: ";		// Demande des informations a l'utilisateur sur le nombre de jours que l'automobile a ÈtÈ louÈe
+	std::cout << "Veuillez entrer le nombre de jours que vous avez lou√© l'automobile: ";		// Demande des informations a l'utilisateur sur le nombre de jours que l'automobile a √©t√© lou√©e
 	std::cin >> jour;
-	std::cout << "Veuillez entrer le nombre de KilomËtres parcourus: ";							// Demande des informations a l'utilisateur sur le nombre de kilomËtre parcouru durant la location
+	std::cout << "Veuillez entrer le nombre de Kilom√®tres parcourus: ";							// Demande des informations a l'utilisateur sur le nombre de kilom√®tre parcouru durant la location
 	std::cin >> kil;
 
 
-	if (kil <= (250 * jour)) // Si la personne a parcouru moin de 250 km par jours
+	if (kil <= (250 * jour)) // FR : Si la personne a parcouru moin de 250 km par jours
 	{
-		std::cout << std::endl << "Le co˚t de location de l'automobile pour la durÈe de la location est de: " << (jour * cout) << " $"; // Affichage du co˚t pour les journÈes
-		std::cout << std::endl << "Le co˚t de l'essence total est de: " << (kil * 0.076) << " $"; //ProblËme rencontrÈ: le total pour le prix (kilomËtre) se dÈfinit automatiquement avec la rÈponse: 62$, peu importe le chiffre entrÈ (rÈsolu), Affichage du co˚t pour l'essence
-		std::cout << std::endl << "Le co˚t total pour la location est donc de: " << ((kil * 0.076) + (cout * jour)) << " $"; // Affichage du co˚t total de la location
+		std::cout << std::endl << "Le co√ªt de location de l'automobile pour la dur√©e de la location est de: " << (jour * cout) << " $"; // Affichage du co√ªt pour les journ√©es
+		
+		//KM : Manque le prix du litre d'essence 1.25 $ 
+		std::cout << std::endl << "Le co√ªt de l'essence total est de: " << (kil * 0.076) << " $"; //Probl√®me rencontr√©: le total pour le prix (kilom√®tre) se d√©finit automatiquement avec la r√©ponse: 62$, peu importe le chiffre entr√© (r√©solu), Affichage du co√ªt pour l'essence
+		std::cout << std::endl << "Le co√ªt total pour la location est donc de: " << ((kil * 0.076) + (cout * jour)) << " $"; // Affichage du co√ªt total de la location
 	}
 
 	else
 	{
-		std::cout << std::endl << "Le co˚t de location de l'automobile pour la durÈe de la location est de: " << (jour * cout) << " $";
-		std::cout << std::endl << "Le co˚t de l'essence total est de: " << (kil * 0.076) << " $"; //ProblËme rencontrÈ: le total pour le prix (kilomËtre) se dÈfinit automatiquement avec la rÈponse: 62$, peu importe le chiffre entrÈ (rÈsolu)
-		std::cout << std::endl << "Le co˚t pour le kilomÈtrage par jours est de: " << ((kil * 0.05) - (12.5 * jour)) << " $"; // Affichage du co˚t pour le kilomÈtrage fait par jour en dÈduisant le 250 kilomËtres gratuits par jour
-		std::cout << std::endl << "Le co˚t total pour la location est donc de: " << ((kil * 0.05) - (12.5 * jour) + (jour * cout) + (kil * 0.076)) << " $"; // Affichage du co˚t total de la location en dÈduisant les kilomËtre gratuit
+		std::cout << std::endl << "Le co√ªt de location de l'automobile pour la dur√©e de la location est de: " << (jour * cout) << " $";
+		std::cout << std::endl << "Le co√ªt de l'essence total est de: " << (kil * 0.076) << " $"; //Probl√®me rencontr√©: le total pour le prix (kilom√®tre) se d√©finit automatiquement avec la r√©ponse: 62$, peu importe le chiffre entr√© (r√©solu)
+		// KM : Je ne comprends pas ce calcul. D'o√π sort le 12.5
+		std::cout << std::endl << "Le co√ªt pour le kilom√©trage par jours est de: " << ((kil * 0.05) - (12.5 * jour)) << " $"; // Affichage du co√ªt pour le kilom√©trage fait par jour en d√©duisant le 250 kilom√®tres gratuits par jour
+		std::cout << std::endl << "Le co√ªt total pour la location est donc de: " << ((kil * 0.05) - (12.5 * jour) + (jour * cout) + (kil * 0.076)) << " $"; // Affichage du co√ªt total de la location en d√©duisant les kilom√®tre gratuit
 	}
 
 
 		/*
 		Plan de test
-		KilomËtre parcouru						Nombre de jours				Co˚t total de la location
-		750										5							287.00$
-		1000									4							256.00$
-		8250									15							1527.00$
+		Kilom√®tre parcouru						Nombre de jours				Co√ªt total de la location		KM : Ce que j'obtiens
+		750									5							287.00$ 	296.25
+		1000									4							256.00$		275	
+		8250									15							1527.00$	1683.75
 		2100									42							2049.60$
 		2313									9							583.938$
 		11250									30							2392.50$		
 		
-		…chec : 4
-		AprËs correction du code (4): RÈussi
+		√âchec : 4
+		Apr√®s correction du code (4): R√©ussi
 		
 		*/ 
 	}
+
+/*
+‚Ä¢	La solution est bien nomm√©e et comporte un projet 5/ 5
+‚Ä¢	Les fichiers sources contiennent l'identification compl√®te du programme et de son auteur	5/5
+‚Ä¢	Le code source est sans erreur de compilation 8/ 15		// KM : Je ne peux pas tester
+‚Ä¢	Le programme est fonctionnel 15/ 20
+‚Ä¢	Le programme passe le plan de tests 15/ 20
+‚Ä¢	Le code source est largement comment√© 20 / 20
+‚Ä¢	La solution est d√©pos√©e dans LEA et dans GitHub.L'adresse est envoy√©e via Outlook	10/15
+	// KM : Pas de solution
+
+
+Note 78/100
+Bon travail !!! REvoir le calcul du km suppl√©mentaire et la remise dans LEA
+*/
 
 
 
